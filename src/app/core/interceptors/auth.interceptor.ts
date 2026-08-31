5963d8a4d940841d8f821706be2962aedbdc9bac
lucasssfer <lucasssfer@hotmail.com>
2026-08-31 19:23:58 -0300

    ajustes

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+﻿import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
